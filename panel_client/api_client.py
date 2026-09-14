4aa623b9312d01787796223d4707c074db5f44db
Luan.nxyl <luannoliveira1455@gmail.com>
2026-09-14 13:17:01 -0400

    Fix bugs

--- panel_client/api_client.py
+++ panel_client/api_client.py
@@ -7,7 +7,10 @@ from typing import Any
 
 import requests
 
-BASE_URL = os.environ.get("PANEL_API_BASE_URL", "https://panel-api-tr1a.onrender.com/")
+BASE_URL = os.environ.get(
+    "PANEL_API_BASE_URL",
+    "https://panel-api-tr1a.onrender.com",
+).strip().rstrip("/")
 _TIMEOUT_SECONDS = 5
 
 
